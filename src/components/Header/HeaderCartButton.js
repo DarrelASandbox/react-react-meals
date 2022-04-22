df242f414a5f9c97c66bec90fa13bcf823e088cc
DarrelA <darrelaiscoding@gmail.com>
2022-04-22 11:58:53 +0800

    bumpAnimation with useEffect(), setTimeout() & clearTimeout()

--- src/components/Header/HeaderCartButton.js
+++ src/components/Header/HeaderCartButton.js
@@ -1,18 +1,27 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/CartProvider';
 import CartIcon from './CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = ({ onShowCart }) => {
+  const [bumpAnimation, setBumpAnimation] = useState(false);
   const cartContext = useContext(CartContext);
-
   const numberOfCartItems = cartContext.items.reduce(
     (currentNumber, item) => currentNumber + item.quantity,
     0
   );
 
+  const btnClasses = `${classes.button} ${bumpAnimation ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (numberOfCartItems === 0) return;
+    setBumpAnimation(true);
+    const timer = setTimeout(() => setBumpAnimation(false), 300);
+    return () => clearTimeout(timer); // clear timer before setting it again
+  }, [numberOfCartItems]);
+
   return (
-    <button className={classes.button} onClick={onShowCart}>
+    <button className={btnClasses} onClick={onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
