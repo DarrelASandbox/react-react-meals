91a6ea9a940c61701e3e5c19dcbd85177c6d60e4
DarrelA <darrelaiscoding@gmail.com>
2022-04-23 22:13:41 +0800

    Fetching meals from firebase

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - React - The Complete Guide (incl Hooks, React Router, Redux)
 - Dive in and learn React.js from scratch! Learn Reactjs, Hooks, Redux, React Routing, Animations, Next.js and way more!
-- Tutorial for Basic age tracker
+- Tutorial for React Meals
 - [Maximilian Schwarzmüller](https://github.com/maxschwarzmueller)
 - [Academind](https://academind.com/)
 
--- src/components/Meals/MealsAvailable.js
+++ src/components/Meals/MealsAvailable.js
@@ -1,38 +1,43 @@
+import { useEffect, useState } from 'react';
 import { Card } from '../UI/';
 import { MealItem } from './Meal';
 import classes from './MealsAvailable.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const MealsAvailable = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} meal={meal} />
-  ));
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchedMeals = [];
+    const fetchMeals = async () => {
+      const responseObject = await (
+        await fetch(`${process.env.REACT_APP_FIREBASE_URI}/meals.json`)
+      ).json();
+
+      for (const key in responseObject) {
+        fetchedMeals.push({
+          id: key,
+          name: responseObject[key].name,
+          description: responseObject[key].description,
+          price: responseObject[key].price,
+        });
+      }
+
+      setMeals(fetchedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals();
+  }, []);
+
+  if (isLoading)
+    return (
+      <section className={classes.spinner}>
+        <p>Loading...</p>
+      </section>
+    );
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
 
   return (
     <section className={classes.meals}>
